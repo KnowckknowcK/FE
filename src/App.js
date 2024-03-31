6070d65c6171e163df5941f707a340e2cfd78440
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-03-31 22:05:45 +0900

    Rename: 토론방 이름에 맞게 변경

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
 import { Routes, Route } from 'react-router-dom'
-import {ChatRoom} from "./page/ChatRoom";
+import {DebateRoom} from "./page/DebateRoom/DebateRoom";
 import {RoomPage} from "./page/RoomPage";
 
 function App() {
   return (
       <Routes>
         <Route path="/room" element={<RoomPage />} />
-        <Route path="/room/:roomId" element={<ChatRoom />} />
+        <Route path="/room/:roomId" element={<DebateRoom />} />
       </Routes>
   );
 }
