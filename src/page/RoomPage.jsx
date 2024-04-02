414d5c049f92dba9b18681f7230531b862605ec5
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-04-02 17:37:00 +0900

    Feat: 토론방 참여 추가

--- src/page/RoomPage.jsx
+++ src/page/RoomPage.jsx
@@ -1,11 +1,17 @@
 import { Link } from 'react-router-dom';
+import {fetchUtil} from "../utils/fetchUtil";
 export function RoomPage() {
+    const getDebateRoomInfo = async (roomId) => {
+        const dto = await fetchUtil(`/debate-room/${roomId}`, {
+            method: 'PUT'
+        });
+    }
     return (
         <>
             <div>토론방 페이지</div>
             <div>
                 {Array.from({length: 5}, (_, i) => i + 1).map(roomId => (
-                    <div key={roomId}>
+                    <div key={roomId} onClick={() => getDebateRoomInfo(roomId)}>
                         <Link to={`/room/${roomId}`}>Room {roomId}</Link>
                     </div>
                 ))}
