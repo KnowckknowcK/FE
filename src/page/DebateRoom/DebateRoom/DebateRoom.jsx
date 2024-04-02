a29f3cd326cba2b2c56e59c5ee5b9ee4d5f0ecae
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-04-02 11:30:27 +0900

    Refactor: DebateRoom에서 커스텀 훅을 useMessage로 옮겨 가독성 향상

--- src/page/DebateRoom/DebateRoom/DebateRoom.jsx
+++ src/page/DebateRoom/DebateRoom/DebateRoom.jsx
@@ -6,48 +6,15 @@ import {MessageItem} from "../MessageItem/MessageItem";
 import {TopNavBar} from "../TopNavBar/TopNavBar";
 import styles from './DebateRoom.module.css';
 import {BottomNavBar} from "../BottomNavBar/BottomNavBar";
+import {useMessages} from "../utils/useMessages";
 
 export function DebateRoom() {
     const stompClient = useStomp();
     let { roomId } = useParams();
     const [yourMessage, setYourMessage] = useState('');
-    const [messages, setMessages] = useState({});
     const [messageThreads, setMessageThreads] = useState({});
-    const [agreeRatio, setAgreeRatio] = useState(0);
-    const [disagreeRatio, setDisagreeRatio] = useState(0);
+    const { messages, agreeRatio, disagreeRatio, handlePutPreference } = useMessages(roomId, stompClient);
 
-    useEffect(() => {
-        const fetchMessages = async () => {
-            const dataList = await fetchUtil(`/message/${roomId}`, {
-                method: 'GET'
-            });// 비동기 결과를 직접 상태에 할당
-
-            const messagesObject = dataList.reduce((acc, cur) => {
-                acc[cur.messageId] = cur;
-                return acc;
-            }, {});
-
-            setMessages(messagesObject);
-        };
-        fetchMessages();
-    }, [roomId]); // roomId를 의존성 배열에 추가
-
-    useEffect(() => {
-        if (!stompClient || stompClient.connected === false) {
-            console.log('Stomp client is not connected. Attempting to reconnect...');
-            stompClient.connect();
-        }
-        const url = `/sub/room/${roomId}`;
-        console.log(`subscribe room: ${roomId}`);
-        const subscription = stompClient.subscribe(url, function (chat) {
-            const message = JSON.parse(chat.body);
-            setMessages((prevMessages) => [...prevMessages, message]);
-        });
-
-        return () => {
-            subscription.unsubscribe();
-        };
-    }, [roomId, stompClient]);
 
     function sendMessage() {
         if (stompClient) {
@@ -65,38 +32,6 @@ export function DebateRoom() {
             ...prevThread,
             [messageId]: prevThreads.data
         }));
-    }
-    // likesNum을 업데이트하는 함수
-    const updateLikesNum = (messageId, newLikesNum) => {
-        setMessages((prevMessages) => ({
-            ...prevMessages,
-            [messageId]: { ...prevMessages[messageId], likesNum: newLikesNum }
-        }));
-    };
-
-    async function handlePutPreference(messageId, position){
-        const isAgree = position !== 'DISAGREE';
-        const dto = await fetchUtil(`/message/preference/${messageId}`, {
-            method: 'PUT',
-            body: {
-                isAgree: isAgree
-            }
-        });
-
-        setAgreeRatio(dto.ratio)
-        if(dto.ratio === 0  && disagreeRatio ===0){
-            setDisagreeRatio(dto.ratio)
-        }else{
-            setDisagreeRatio(100 - dto.ratio)
-        }
-
-        let newLikesNum = messages[messageId].likesNum;
-        if(dto.isIncrease){
-            newLikesNum += 1;
-        }else{
-            newLikesNum -= 1;
-        }
-        updateLikesNum(messageId, newLikesNum);
     }
     return (
         <div>
---
+++ src/page/DebateRoom/utils/useMessages.jsx
@@ -0,0 +1,93 @@
+// hooks/useMessages.js
+import { useState, useEffect } from 'react';
+import { fetchUtil } from "../../../utils/fetchUtil";
+
+export const useMessages = (roomId, stompClient) => {
+    const [messages, setMessages] = useState({});
+    const [agreeRatio, setAgreeRatio] = useState(0);
+    const [disagreeRatio, setDisagreeRatio] = useState(0);
+
+    // 메세지 받아오기
+    useEffect(() => {
+        const fetchMessages = async () => {
+            const dataList = await fetchUtil(`/message/${roomId}`, {
+                method: 'GET'
+            });
+
+            const messagesObject = dataList.reduce((acc, cur) => {
+                acc[cur.messageId] = cur;
+                return acc;
+            }, {});
+
+            setMessages(messagesObject);
+        };
+        fetchMessages();
+    }, [roomId]);
+
+    // 해당 메세지 구독 -> 실시간 채팅 제공
+    useEffect(() => {
+        if (!stompClient || stompClient.connected === false) {
+            console.log('Stomp client is not connected. Attempting to reconnect...');
+            stompClient.connect();
+        }
+        const url = `/sub/room/${roomId}`;
+        console.log(`subscribe room: ${roomId}`);
+        const subscription = stompClient.subscribe(url, function (chat) {
+            const message = JSON.parse(chat.body);
+            setMessages((prevMessages) => [...prevMessages, message]);
+        });
+
+        return () => {
+            subscription.unsubscribe();
+        };
+    }, [roomId, stompClient]);
+
+    // 토론방 입장 시 찬/반 비율 보여줌 -> 수정 필요 현재 받고 있는 있는 ratio가 잘못됨(좋아요 비율이 바뀌는 중)
+    useEffect(()=>{
+        const getRatio = async () => {
+            const curRatio = await fetchUtil(`/debate-room/${roomId}`, {
+                method: 'PUT'
+            });
+            setAgreeRatio(curRatio);
+        }
+        getRatio();
+    }, [])
+
+
+    // 좋아요 추가 로직
+    async function handlePutPreference(messageId, position){
+        const isAgree = position !== 'DISAGREE';
+        const dto = await fetchUtil(`/message/preference/${messageId}`, {
+            method: 'PUT',
+            body: {
+                isAgree: isAgree
+            }
+        });
+
+        updateRatio(dto.ratio);
+        updateLikesNum(messageId, dto.isIncrease);
+    }
+
+    function updateRatio(ratio){
+        setAgreeRatio(ratio);
+        if(ratio === 0  && disagreeRatio ===0){
+            setDisagreeRatio(ratio)
+        }else{
+            setDisagreeRatio(100 - ratio)
+        }
+    }
+
+    function updateLikesNum(messageId, isIncrease){
+        let newLikesNum = messages[messageId].likesNum;
+        if(isIncrease){
+            newLikesNum += 1;
+        }else{
+            newLikesNum -= 1;
+        }
+        setMessages((prevMessages) => ({
+            ...prevMessages,
+            [messageId]: { ...prevMessages[messageId], likesNum: newLikesNum }
+        }));
+    };
+    return { messages, agreeRatio, disagreeRatio, handlePutPreference };
+};
