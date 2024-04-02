51db5e7f062f19caf1abacf4cb806c9fd022e504
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-04-02 17:36:44 +0900

    Fix: 토론방 참여 시 찬/반 인원 정상적으로 보이게 수정

--- src/page/DebateRoom/TopNavBar/TopNavBar.jsx
+++ src/page/DebateRoom/TopNavBar/TopNavBar.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { FaArrowLeft } from 'react-icons/fa';
 import styles from './TopNavBar.module.css'; // CSS 모듈 임포트
+import { useNavigate } from 'react-router-dom';
 
-export const TopNavBar = ({ roomNumber, participantCount, agreeRate, disagreeRate }) => {
+export const TopNavBar = ({ roomNumber, agreeNum, disagreeNum, agreeRate, disagreeRate }) => {
+    const navigate = useNavigate();
     return (
         <div className={styles.navBarStyle}>
             <div className={styles.itemStyle}>
-                <FaArrowLeft /> {/* 나가기 아이콘 */}
+                <button onClick={() => navigate(-1)}> {/* 버튼 클릭 시 이전 페이지로 이동 */}
+                    <FaArrowLeft/>
+                </button>
                 <div className={styles.iconTextStyle}>
                     <div>{`${roomNumber}번 토론방`}</div>
-                    <div className={styles.smallText}>{`${participantCount}명의 참여자`}</div>
+                    <div className={styles.smallText}>{`찬성: ${agreeNum}명 반대: ${disagreeNum}`}</div>
                 </div>
             </div>
             <div>
