9f6c03cf11bff10ca5c7481a9a5aa69e648a0bb3
adorableco <tpdusdl1001@knu.ac.kr>
2024-04-12 22:28:56 +0900

    Feat: 지문 목록 조회 페이지네이션/카테고리 추가
    - 선택된 카테고리 버튼의 색상을 변화시키는 것을 추가해야 함
    
    Related to: #8

--- src/page/literacy/ArticleList.jsx
+++ src/page/literacy/ArticleList.jsx
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from "react";
 import styles from "./ArticleList.module.css";
 import Article from "../literacy/Article";
 import axios from "axios";
+import Pagination from '@mui/material/Pagination';
 
 const api = process.env.REACT_APP_API_URL;
 
@@ -9,27 +10,36 @@ const ArticleList = () => {
 
     const [articleList, setArticleList] = useState([]);
     const [isLoading, setIsLoading] = useState([true]);
-    const [category, setCategory] = useState("economics")
+    const [category, setCategory] = useState("POLITICS");
+    const [pageNum, setPageNum] = useState(0);
+
 
     useEffect(() => {
         const loadArticle = async () => {
             return await axios
-              .get(api + `/api/article/list`)
+              .get(api + `/api/article/list/${category}/${pageNum}`)
               .then((response) => {
-                setArticleList(response.data);
+                setArticleList(response.data.content);
               });
           };
       
           const fetchData = async () => {
-            console.log("here")
             await loadArticle();
-            console.log(articleList)
             setIsLoading(false);
           };
       
           fetchData();
           
-    },[])
+    },[category, pageNum])
+
+    const handlePageChange = (e) => {
+        const nowPageInt = parseInt(e.target.outerText);
+        setPageNum(nowPageInt-1);
+      };
+
+      const onclickCategory = (category) => {
+        setCategory(category);
+      }
 
     
     if (isLoading) return <p>로딩중...</p>
@@ -37,15 +47,22 @@ const ArticleList = () => {
     return (
         <div>
             <div className={styles.menuBtn}>
-                <btn className={styles.menu}>경제</btn>
-                <btn className={styles.menu}>정치</btn>
-                <btn className={styles.menu}>사회</btn>
+                <button className={styles.menu} onClick={() => onclickCategory("ECONOMICS")}>경제</button>
+                <button className={styles.menu} onClick={() => onclickCategory("POLITICS")}>정치</button>
+                <button className={styles.menu} onClick={() => onclickCategory("SOCIAL")}>사회</button>
+                <button className={styles.menu} onClick={() => onclickCategory("CULTURE")}>문화</button>
+                <button className={styles.menu} onClick={() => onclickCategory("WORLD")}>세계</button>
+                <button className={styles.menu} onClick={() => onclickCategory("IT")}>IT</button>
+                <button className={styles.menu} onClick={() => onclickCategory("IT")}>IT</button>
+                <button className={styles.menu} onClick={() => onclickCategory("ENTERTAINMENT")}>연예</button>
+                <button className={styles.menu} onClick={() => onclickCategory("SPORT")}>스포츠</button>
             </div>
             <div className={styles.articleList}>
-                {articleList.map((article) => (
+                {articleList.length !== 0  && articleList.map((article) => (
                     <Article data = {article}/>
                 ))}
             </div>
+            <Pagination  count={5} shape="rounded" onChange={(e) => handlePageChange(e)}/>
         </div>
     )
 }
