6631233dbe12521d295862c7ea2f0a959688b2ab
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-04-21 21:54:58 +0900

    Design: 토론방 네비게이션 바 UI 적용

--- src/page/DebateRoom/Common/TopNavBar/TopNavBar.jsx
+++ src/page/DebateRoom/Common/TopNavBar/TopNavBar.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { FaArrowLeft } from 'react-icons/fa';
 import styles from './TopNavBar.module.css';
 import {GiHamburgerMenu} from "react-icons/gi"; // CSS 모듈 임포트
-
+import { RxHamburgerMenu } from "react-icons/rx";
 
 export const TopNavBar = ({handleOnClick, isMain, toggleDrawer, children }) => {
 
     return (
-        <div className={styles.navBarStyle}>
+        <div className={styles.topNavBarStyle}>
             <div className={styles.itemStyle}>
                 <button onClick={() => handleOnClick()}> {/* 버튼 클릭 시 이전 페이지로 이동 */}
                     <FaArrowLeft/>
@@ -16,7 +16,11 @@ export const TopNavBar = ({handleOnClick, isMain, toggleDrawer, children }) => {
                     {children}
                 </div>
             </div>
-            {isMain && (<div onClick={toggleDrawer}><GiHamburgerMenu/></div>)}
+            {isMain &&
+                (<div className={styles.drawer} onClick={toggleDrawer}>
+                    <RxHamburgerMenu style={{color: "white", height:"36px", width:"36px"}}/>
+                </div>)
+            }
         </div>
     );
 };
