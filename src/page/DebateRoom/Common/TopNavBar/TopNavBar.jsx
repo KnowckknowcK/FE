3e02fb2a91d5244f5d9b9f1baef2c19c8e3f853f
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-04-22 20:12:59 +0900

    Design: 상단 네비게이션바 나가기 버튼 변경

--- src/page/DebateRoom/Common/TopNavBar/TopNavBar.jsx
+++ src/page/DebateRoom/Common/TopNavBar/TopNavBar.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
-import { FaArrowLeft } from 'react-icons/fa';
 import styles from './TopNavBar.module.css';
-import {GiHamburgerMenu} from "react-icons/gi"; // CSS 모듈 임포트
 import { RxHamburgerMenu } from "react-icons/rx";
+import { FiChevronLeft } from "react-icons/fi";
 
 export const TopNavBar = ({handleOnClick, isMain, toggleDrawer, children }) => {
 
     return (
         <div className={styles.topNavBarStyle}>
             <div className={styles.itemStyle}>
-                <button onClick={() => handleOnClick()}> {/* 버튼 클릭 시 이전 페이지로 이동 */}
-                    <FaArrowLeft/>
-                </button>
+                <div onClick={() => handleOnClick()}> {/* 버튼 클릭 시 이전 페이지로 이동 */}
+                    <FiChevronLeft style={{color:"white",width:"36px", height:"36px"}}/>
+                </div>
                 <div className={styles.iconTextStyle}>
                     {children}
                 </div>
