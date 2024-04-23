c6afece474da8f0e29a6c371171dc44cdac8685a
kwonssshyeon <kwonsh4396@gmail.com>
2024-04-23 16:43:55 +0900

    Feat: 추천기사 api 호출 기능 구현 및 horizontal scroll 적용

--- src/page/main/Main.jsx
+++ src/page/main/Main.jsx
@@ -1,8 +1,84 @@
-import React from "react";
-import RecommendedItem from "./RecommendedItem";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import RecommendedItem from './RecommendedItem';
+import styled from "styled-components";
 
 function Main(props) {
-    return <RecommendedItem />
+    const [data, setData] = useState([]);
+  	
+  	useEffect(() => {
+		const fetchData = async() => {
+          const res = await axios.get('http://api.knowckknowck.com:8080/api/article/recommand');
+          return res.data;
+        }	
+        
+        fetchData().then(
+            res => setData(res.data));
+    }, []);
+
+
+    return (
+        <div>
+
+        <Wrapper>
+            <Intro>
+                똑똑과 함께 <br/> AI 피드백 받고, <br/> 문해력을 향상해보세요<br/> 
+            </Intro>
+        </Wrapper>
+        <Subtitle1>오늘의 맞춤 추천 기사</Subtitle1>
+        <RecommandList>
+            {data.map((item, index) =>(
+                <RecommendedItem title={item.title} category={item.category} />
+            ))}
+        </RecommandList>
+        <SelectButton>
+
+        </SelectButton>
+
+        </div>
+    )
+    
 }
+
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    background-color: #E3D5CA;
+`;
+
+const Intro = styled.h2`
+    color: white;
+    padding: 20px;
+`;
+
+const Subtitle1 = styled.div`
+    font-weight: bold;
+    font-size: '0.875rem';
+    padding: 10px;
+`;
+
+const RecommandList = styled.div`
+    padding-left: 10px;
+    padding-right: 10px;
+    width: 100%;
+    display: flex;
+    gap: 32px;
+    overflow-x: auto;
+    scrollbar-color: transparent transparent;
+    user-select: none;
+    &::-webkit-scrollbar {
+        height: 1px;
+        background-color: transparent;
+    }
+`
+
+const SelectButton = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    background-color: #E3D5CA;
+`
 
 export default Main;
--- src/page/main/RecommendedItem.jsx
+++ src/page/main/RecommendedItem.jsx
@@ -4,25 +4,55 @@ import styled from "styled-components";
 
 function RecommendedItem(props) {
     return (
-        <Wrapper>
-            <Intro>
-                똑똑과 함께 <br/> AI 피드백 받고, <br/> 문해력을 향상해보세요<br/> 
-            </Intro>
-        </Wrapper>
         
+        <div>
+            <Wrapper>
+                <Category>{`${props.category}`}</Category>
+                <Title>{`${props.title}`}</Title>
+                <Button>문해력 진단 시작하기</Button>
+            </Wrapper>
+        </div>
+
     )
 }
 
+
 const Wrapper = styled.div`
+    padding: 15px;
+    width: 200px;
+    height: 250px;
+    border-radius: 20px;
     display: flex;
-    flex-direction: row;
+    flex-direction: column;
     align-items: flex-start;
-    background-color: #E3D5CA;
+    background-color: #6B9080;
 `;
 
-const Intro = styled.h2`
-    // Global style 에서 바꿀 부분 정의
-    color: white;
+const Category = styled.div`
+    position: relative;
+    top: 10px;
+    font-size: 0.75rem;
 `;
+
+const Title = styled.div`
+    position: relative;
+    top: 10px;
+    height: 80%;
+    font-weight: bold;
+    font-size: 1rem;
+`;
+
+const Button = styled.button`
+    margin: 0 auto;
+    padding: 10px;
+    border: none;
+    border-radius: 30px;
+    position: relative;
+    font-weight: bold;
+    font-size: 0.875rem;
+    color: #6B9080;
+    background-color: #E8EBEA;
+`;
+
 
 export default RecommendedItem;
