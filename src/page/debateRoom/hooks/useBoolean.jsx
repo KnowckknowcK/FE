861d2edb0aeb89f631f4321c127458d18af03c80
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-05-17 10:33:12 +0900

    refactor: 토론방 로직 전체 리팩토링

--- src/page/debateRoom/hooks/useBoolean.jsx
+++
@@ -1,6 +0,0 @@
-import {useState} from "react";
-
-export function useBoolean() {
-    const { isTrue, setIsTrue } = useState(null)
-    return {isTrue, setIsTrue};
-}
--- src/page/debateRoom/hooks/useSubscribe.jsx
+++ src/page/debateRoom/hooks/useSubscribe.jsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from "react";
+import {useEffect} from "react";
 import {useStomp} from "../../../context/StompContext";
 
 export function useSubscribe(roomId, setMessages, isThread, messageId) {
