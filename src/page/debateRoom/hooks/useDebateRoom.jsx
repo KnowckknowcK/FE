6f716d8c94c1ea3ce88c67e15994332a27c3d808
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-05-17 13:45:49 +0900

    refactor: 토론방 로직 전체 리팩토링

--- src/page/debateRoom/hooks/useDebateRoom.jsx
+++ src/page/debateRoom/hooks/useDebateRoom.jsx
@@ -2,8 +2,8 @@ import {useEffect, useState} from "react";
 import CustomAxios from "../../../lib/customAxios";
 
 export function useDebateRoom(roomId) {
-    const {debateRoomInfo, setDebateRoomInfo} = useState(null)
-    const {agreeRatio, setAgreeRatio} = useState(0)
+    const [debateRoomInfo, setDebateRoomInfo] = useState(null)
+    const [agreeRatio, setAgreeRatio] = useState(0)
     const [disagreeRatio, setDisagreeRatio] = useState(0);
 
     useEffect(() => {
@@ -11,7 +11,8 @@ export function useDebateRoom(roomId) {
             try {
                 const response = await CustomAxios.put(`/debate-room/${roomId}`, null);
                 // response에서 원하는 데이터 추출하여 상태 업데이트
-                setDebateRoomInfo(response.data.data);
+                const dto = response.data.data;
+                setDebateRoomInfo(dto);
             } catch (error) {
                 console.error('Debate room 정보를 가져오는 중 오류가 발생했습니다.', error);
             }
