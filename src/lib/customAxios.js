61322fcbf70105da1f28dd0c9e527bd04ad0762a
kwonssshyeon <kwonsh4396@gmail.com>
2024-05-27 17:46:25 +0900

    Fix: 401 리다이렉트 로그인 페이지로 변경

--- src/lib/customAxios.js
+++ src/lib/customAxios.js
@@ -1,50 +1,3 @@
-// /** @format */
-// import axios from "axios";
-//
-// const { REACT_APP_API_URL } = process.env;
-//
-// const getToken = () => {
-//   return localStorage.getItem('accessToken');
-// };
-//
-//
-// const customAxios = axios.create({
-//   baseURL: REACT_APP_API_URL + "/api",
-//   headers: {
-//     Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
-//   }
-// });
-//
-//
-//
-// // customAxios.interceptors.request.use(
-// //   (config) => {
-// //     const token = getToken();
-// //     if (token) {
-// //       config.headers.Authorization = `Bearer ${token}`;
-// //     } else {
-// //       config.headers.Authorization = null;
-// //     }
-// //     return config;
-// //   }
-// // );
-// //
-// //
-// // customAxios.interceptors.response.use(
-// //   response => response,
-// //   error => {
-// //     if (error.response && error.response.status === 401) {
-// //       alert("로그인 후 시도해주세요.");
-// //       window.location.href = '/signin';
-// //       return new Promise(() => {});
-// //     }
-// //
-// //     return Promise.reject(error);
-// //   }
-// // );
-//
-// export default customAxios;
-
 /** @format */
 import axios from "axios";
 
@@ -79,7 +32,7 @@ customAxios.interceptors.response.use(
   (error) => {
     if (error.response && error.response.status === 401) {
       alert("로그인 후 시도해주세요.");
-      window.location.href = "/mypage";
+      window.location.href = "/signin";
       return new Promise(() => {});
     }
 
